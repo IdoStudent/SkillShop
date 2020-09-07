d1c0f3fd9f53c5d38a690a64aad6db48b10fefc9
Nick Linney-Barber <s3660035@student.rmit.edu.au>
2020-09-07 16:15:23 +1000

    Cleaned up the site wrapper a bit for demo

--- react-app/src/SiteWrapper.react.js
+++ react-app/src/SiteWrapper.react.js
@@ -51,11 +51,6 @@ const navBarItems: Array<navItem> = [
     LinkComponent: withRouter(NavLink),
     useExact: true,
   },
-  {
-    value: "Home (Employer - Placeholder)",
-    icon: "home",
-    to: "/EmployerSetup",
-    LinkComponent: withRouter(NavLink),
 
     /* Example of how to use a sub-nav dropdown (if we need it)
     subItems: [
@@ -72,7 +67,6 @@ const navBarItems: Array<navItem> = [
       },
     ],
     */
-  },
 ];
 
 const accountDropdownProps = {
@@ -80,12 +74,9 @@ const accountDropdownProps = {
   name: "Jane Pearson",
   description: "Administrator",
   options: [
-    { icon: "user", value: "Profile" },
-    { icon: "settings", value: "Settings" },
+    { icon: "user", value: "Profile", to: "/", LinkComponent: withRouter(NavLink) },
     { icon: "mail", value: "Inbox", badge: "6" },
-    { icon: "send", value: "Message" },
     { isDivider: true },
-    { icon: "help-circle", value: "Need help?" },
     { icon: "log-out", value: "Sign out" },
   ],
 };
@@ -95,31 +86,30 @@ class SiteWrapper extends React.Component<Props, State> {
     notificationsObjects: [
       {
         unread: true,
-        avatarURL: "demo/faces/male/41.jpg",
+        //avatarURL: "demo/faces/male/41.jpg",
         message: (
           <React.Fragment>
-            <strong>Nathan</strong> pushed new commit: Fix page load performance
-            issue.
-          </React.Fragment>
+            You have a new match! <strong>Marketing Consultant</strong> at <strong>Deloitte</strong>
+          </React.Fragment> 
         ),
         time: "10 minutes ago",
       },
       {
         unread: true,
-        avatarURL: "demo/faces/female/1.jpg",
+        //avatarURL: "demo/faces/female/1.jpg",
         message: (
           <React.Fragment>
-            <strong>Alice</strong> started new task: Tabler UI design.
-          </React.Fragment>
+            You have a new match! <strong>Creative Intern</strong> at <strong>Envato</strong>
+          </React.Fragment> 
         ),
         time: "1 hour ago",
       },
       {
         unread: false,
-        avatarURL: "demo/faces/female/18.jpg",
+        //avatarURL: "demo/faces/female/18.jpg",
         message: (
           <React.Fragment>
-            <strong>Rose</strong> deployed new version of NodeJS REST Api // V3
+            You've received a new message.  <u>Click to view</u>
           </React.Fragment>
         ),
         time: "2 hours ago",
