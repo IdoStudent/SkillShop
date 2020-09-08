981142fb3d7a0aec2985cc90b4bb5b919a61b3f6
Cassius Hojmark <37318518+Cassius88@users.noreply.github.com>
2020-09-08 16:44:04 +1000

    userEmail parma for api call

--- react-app/src/components/JobseekerProfile/GeneralInformation.react.js
+++ react-app/src/components/JobseekerProfile/GeneralInformation.react.js
@@ -2,9 +2,10 @@ import * as React from "react";
 import axios from "axios";
 import { Form, Card, Grid } from "tabler-react";
 import { Button, Modal } from "semantic-ui-react";
-import Auth from "@aws-amplify/auth";
+import Auth from '@aws-amplify/auth';
 
-import "../../index.css";
+import '../../index.css';
+import { arrayOf } from "prop-types";
 
 //const config = require('../config.json');
 
@@ -19,6 +20,7 @@ class GeneralInformation extends React.Component {
       city: "",
       postcode: "",
       state: "",
+      email: "",
       about: "",
 
       // States for editable form
@@ -32,89 +34,56 @@ class GeneralInformation extends React.Component {
 
       // Modal State
       open: false,
-
-      // Form Validation stuff
-      fNameInvalid: false,
-      fNameErrorMsg: "",
-
-      surnameInvalid: false,
-      surnameErrorMsg: "",
-
-      postcodeInvalid: false,
-      postcodeErrorMsg: "",
-
-      aboutInvalid: false,
-      aboutErrorMsg: "",
     };
   }
-
-  getFirstApi() {
-    return Auth.currentAuthenticatedUser().then((user) => {
-      this.setState({
-        email: user.attributes.email,
-        formemail: user.attributes.email,
-      });
+  // GET email for getSecondApi 
+  getEmailApi() {
+   return Auth.currentAuthenticatedUser().then((user) => {
+      const { attributes = {} } = user;
+      console.log(attributes['email']);
+      let email =  attributes['email']
+      return email
+    })}
+  // GET email for form
+ getFirstApi() {
+   return Auth.currentAuthenticatedUser().then((user) => {
+      this.setState({email: user.attributes.email, formemail: user.attributes.email})
     });
+ }
+ // GET user data 
+  async getSecondApi(email) {
+    fetch(`https://ezha2ns0bl.execute-api.ap-southeast-2.amazonaws.com/prod/userdata?userEmail=` +email)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            firstname: result.Item.userFirstName,
+            middlename: result.Item.userMiddleName,
+            surname: result.Item.userLastName,
+            city: result.Item.userCity,
+            postcode: result.Item.userPostcode,
+            state: result.Item.userState,
+            about: result.Item.userAbout,
+
+            formfirstname: result.Item.userFirstName,
+            formmiddlename: result.Item.userMiddleName,
+            formsurname: result.Item.userLastName,
+            formcity: result.Item.userCity,
+            formpostcode: result.postcode,
+            formstate: result.Item.userState,
+            formabout: result.Item.userAbout,
+            
+          });
+          console.log("THIS IS RESULT1 " + this.state.email)} ,
+      )
   }
+  // pass before mount
+  BeforDidMount() { 
+   this.getEmailApi().then(email => this.getSecondApi(email)); }
 
-  //${encodeURIComponent(data.foo)}
-  getSecondApi(email) {
-    fetch(
-      `https://ezha2ns0bl.execute-api.ap-southeast-2.amazonaws.com/prod/userdata?foo=${encodeURIComponent(
-        email
-      )}`
-    )
-      .then((res) => res.json())
-      .then(console.log("THIS IS RESULT2 " + email), (result) => {
-        this.setState({
-          firstname: result.Item.userFirstName,
-          middlename: result.Item.userMiddleName,
-          surname: result.Item.userLastName,
-          city: result.Item.userCity,
-          postcode: result.Item.userPostcode,
-          state: result.Item.userState,
-          about: result.Item.userAbout,
-
-          formfirstname: result.Item.userFirstName,
-          formmiddlename: result.Item.userMiddleName,
-          formsurname: result.Item.userLastName,
-          formcity: result.Item.userCity,
-          formpostcode: result.postcode,
-          formstate: result.Item.userState,
-          formabout: result.Item.userAbout,
-        });
-        console.log("THIS IS RESULT1 " + result);
-      });
-  }
-
-  //${encodeURIComponent(data.foo)}
-  getSecondApi(email) {
-    fetch(
-      `https://ezha2ns0bl.execute-api.ap-southeast-2.amazonaws.com/prod/userdata?foo=${encodeURIComponent(
-        email
-      )}`
-    )
-      .then((res) => res.json())
-      .then(console.log("THIS IS RESULT2 " + email), (result) => {
-        this.setState({
-          firstname: result.Item.userFirstName,
-          middlename: result.Item.userMiddleName,
-          surname: result.Item.userLastName,
-          city: result.Item.userCity,
-          postcode: result.Item.userPostcode,
-          state: result.Item.userState,
-          about: result.Item.userAbout,
-
-          formfirstname: result.Item.userFirstName,
-          formmiddlename: result.Item.userMiddleName,
-          formsurname: result.Item.userLastName,
-          formcity: result.Item.userCity,
-          formpostcode: result.postcode,
-          formstate: result.Item.userState,
-          formabout: result.Item.userAbout,
-        });
-        console.log("THIS IS RESULT1 " + result);
-      });
+  componentDidMount() {
+    this.BeforDidMount();
+    this.getFirstApi();  
   }
 
   handleChange = (input) => (event) => {
@@ -124,114 +93,36 @@ class GeneralInformation extends React.Component {
   handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (this.validateForm()) {
-      this.setState((prevState) => ({
-        // If submitting new values, update the state to represent the new data
-        firstname: prevState.formfirstname,
-        middlename: prevState.formmiddlename,
-        surname: prevState.formsurname,
-        city: prevState.formcity,
-        postcode: prevState.formpostcode,
-        userState: prevState.formstate,
-        about: prevState.formabout,
-        open: false,
-      }));
-      try {
-        const params = {
-          userEmail: "shaahin@gmail.com",
-          userFirstName: this.state.formfirstname,
-          userMiddleName: this.state.formmiddlename,
-          userLastName: this.state.formsurname,
-          userCity: this.state.formcity,
-          userPostCode: this.state.formpostcode,
-          userState: this.state.formstate,
-          userAbout: this.state.formabout,
-          userType: "jobseeker",
-        };
-        await axios.post(
-          "https://qrg3idkox4.execute-api.ap-southeast-2.amazonaws.com/prod/{userEmail}/",
-          params
-        );
-      } catch (err) {
-        console.log(`An error has occurred: ${err}`);
-      }
-    }
-  };
-
-  validateForm = () => {
-    let fName = this.state.formfirstname;
-    let surname = this.state.formsurname;
-    let postcode = this.state.formpostcode;
-    let about = this.state.formabout;
-
-    this.setState({
-      fNameInvalid: false,
-      surnameInvalid: false,
-      postcodeInvalid: false,
-      aboutInvalid: false,
-    });
-
-    let validInput = true;
-
-    if (!fName) {
-      this.setState({
-        fNameErrorMsg: "First name cannot be empty",
-        fNameInvalid: true,
-      });
-      validInput = false;
-    } else if (fName.length < 2) {
-      this.setState({
-        fNameErrorMsg: "First name needs to be 2 or more characters",
-        fNameInvalid: true,
-      });
-      validInput = false;
-    }
-
-    if (!surname) {
-      this.setState({
-        surnameErrorMsg: "Surname cannot be empty",
-        surnameInvalid: true,
-      });
-      validInput = false;
-    } else if (surname.length < 2) {
-      this.setState({
-        surnameErrorMsg: "Surname needs to be 2 or more characters",
-        surnameInvalid: true,
-      });
-      validInput = false;
-    }
-
-    if (!postcode) {
-      this.setState({
-        postcodeErrorMsg: "Postcode cannot be empty",
-        postcodeInvalid: true,
-      });
-      validInput = false;
-    } else if (postcode.includes("_")) {
-      this.setState({
-        postcodeErrorMsg: "Please fill out the full postcode",
-        postcodeInvalid: true,
-      });
-      validInput = false;
-    }
-
-    if (!about) {
-      this.setState({
-        aboutErrorMsg: "Description cannot be empty",
-        aboutInvalid: true,
-      });
-      validInput = false;
-    } else if (about.length < 100) {
-      this.setState({
-        aboutErrorMsg: "Tell us more about you! (100+ characters)",
-        aboutInvalid: true,
-      });
-      validInput = false;
-    }
-
-    // Return the status of valid input. If any of the above error conditions are met, this will return false
-    return validInput;
-  };
+    this.setState((prevState) => ({
+      // If submitting new values, update the state to represent the new data
+      firstname: prevState.formfirstname,
+      middlename: prevState.formmiddlename,
+      surname: prevState.formsurname,
+      city: prevState.formcity,
+      postcode: prevState.formpostcode,
+      email: prevState.formemail,
+      userState: prevState.formstate,
+      about: prevState.formabout,
+      open: false,
+      
+    }))
+    try {
+      const params = {
+        "userFirstName": this.state.formfirstname,
+        "userMiddleName": this.state.formmiddlename,
+        "userLastName": this.state.formsurname,
+        "userCity": this.state.formcity,
+        "userPostCode": this.state.formpostcode,
+        "userEmail": this.state.formemail,
+        "userState": this.state.formstate,
+        "userAbout": this.state.formabout,
+        "userType": "jobseeker"
+      };
+      await axios.post('https://qrg3idkox4.execute-api.ap-southeast-2.amazonaws.com/prod/{userEmail}/', params);
+    }catch (err) {
+      console.log(`An error has occurred: ${err}`);
+  }
+ };
 
   cancelForm = () => {
     // If cancelling, reset any fields that have been changed to the original values so that when the modal is re-opened, the old values are shown
@@ -259,6 +150,7 @@ class GeneralInformation extends React.Component {
       city,
       postcode,
       state,
+      email,
       about,
       formfirstname,
       formmiddlename,
@@ -325,6 +217,11 @@ class GeneralInformation extends React.Component {
                   <Form.Input name="state" readOnly value={state} />
                 </Form.Group>
               </Grid.Col>
+              <Grid.Col md={4}>
+                <Form.Group label="Email">
+                  <Form.Input name="email" readOnly value={email} />
+                </Form.Group>
+              </Grid.Col>
 
               {/* ROW 3 */}
               <Grid.Col md={12}>
@@ -350,16 +247,14 @@ class GeneralInformation extends React.Component {
         >
           <Modal.Header>Edit Info</Modal.Header>
           <Modal.Content>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
               <Grid.Row>
                 <Grid.Col md={4}>
-                  <Form.Group label="First Name" isRequired>
+                  <Form.Group label="First Name">
                     <Form.Input
                       name="firstname"
                       value={formfirstname}
                       onChange={this.handleChange("formfirstname")}
-                      invalid={this.state.fNameInvalid}
-                      feedback={this.state.fNameErrorMsg}
                     />
                   </Form.Group>
                 </Grid.Col>
@@ -373,13 +268,11 @@ class GeneralInformation extends React.Component {
                   </Form.Group>
                 </Grid.Col>
                 <Grid.Col md={4}>
-                  <Form.Group label="Surname" isRequired>
+                  <Form.Group label="Surname">
                     <Form.Input
                       name="surname"
                       value={formsurname}
                       onChange={this.handleChange("formsurname")}
-                      invalid={this.state.surnameInvalid}
-                      feedback={this.state.surnameErrorMsg}
                     />
                   </Form.Group>
                 </Grid.Col>
@@ -397,33 +290,21 @@ class GeneralInformation extends React.Component {
                   </Form.Group>
                 </Grid.Col>
                 <Grid.Col md={2}>
-                  <Form.Group label="Post Code" isRequired>
-                    <Form.MaskedInput
-                      placeholder="0000"
-                      mask={[/\d/, /\d/, /\d/, /\d/]}
-                      name="startdate"
+                  <Form.Group label="Post Code">
+                    <Form.Input
+                      name="postcode"
                       value={formpostcode}
                       onChange={this.handleChange("formpostcode")}
-                      invalid={this.state.postcodeInvalid}
-                      feedback={this.state.postcodeErrorMsg}
                     />
                   </Form.Group>
                 </Grid.Col>
                 <Grid.Col md={3}>
-                  <Form.Group label="State" isRequired>
-                    <Form.Select
+                  <Form.Group label="State">
+                    <Form.Input
                       name="state"
                       value={formstate}
                       onChange={this.handleChange("formstate")}
-                    >
-                      <option value="NSW">NSW</option>
-                      <option value="NT">NT</option>
-                      <option value="QLD">QLD</option>
-                      <option value="SA">SA</option>
-                      <option value="TAS">TAS</option>
-                      <option value="VIC">VIC</option>
-                      <option value="WA">WA</option>
-                    </Form.Select>
+                    />
                   </Form.Group>
                 </Grid.Col>
               </Grid.Row>
@@ -431,14 +312,12 @@ class GeneralInformation extends React.Component {
               {/* ROW 3 */}
               <Grid.Row>
                 <Grid.Col md={12}>
-                  <Form.Group className="mb=0" label="About Me" isRequired>
+                  <Form.Group className="mb=0" label="About Me">
                     <Form.Textarea
                       name="aboutme"
                       rows={3}
                       value={formabout}
                       onChange={this.handleChange("formabout")}
-                      invalid={this.state.aboutInvalid}
-                      feedback={this.state.aboutErrorMsg}
                     />
                   </Form.Group>
                 </Grid.Col>
@@ -457,13 +336,7 @@ class GeneralInformation extends React.Component {
                     {" "}
                     Cancel{" "}
                   </Button>
-                  <Button
-                    floated="right"
-                    basic
-                    type="button"
-                    color="green"
-                    onClick={this.handleSubmit}
-                  >
+                  <Button floated="right" basic type="submit" color="green">
                     {" "}
                     Accept Changes{" "}
                   </Button>
