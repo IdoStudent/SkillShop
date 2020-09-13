3f4da625c64cff4bf3b3ea310505c242bb9533b9
IdoStudent <41351620+IdoStudent@users.noreply.github.com>
2020-09-13 13:05:51 +1000

    chat input and send button

--- react-app/src/pages/Chat.react.js
+++ react-app/src/pages/Chat.react.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
+import { Container, Grid, Header, Button } from "tabler-react";
+import SiteWrapper from "../SiteWrapper.react";
 
 class Chat extends Component {
     render(){
         return(
-            <h1>Chat</h1>
+            <SiteWrapper>
+                <div className="my-body">
+                    <div className="container"> 
+                        <div className="row my-row chat-box">
+                            <div className="col my-col">
+                                IDO
+                            </div>
+                            <div className="col my-col">
+                                YARON
+                            </div>
+                        </div>
+                        <div className="row text-box">
+                            <div className="col-11">
+                                <input className="input-text" type="text" placeholder="Enter your message here"></input>
+                            </div>
+                            <div className="col-1">
+                                <button className="my-button">
+                                    SEND
+                                </button>
+                            </div>
+                            
+                        </div>               
+                    </div>
+                </div>
+                
+            </SiteWrapper>
         )
     }
 }
