4eab54df15ffcc37cf0d528d66b8366c3b9f7194
Nick Linney-Barber <s3660035@student.rmit.edu.au>
2020-09-23 10:40:55 +1000

    messed up the merge recommitting

--- react-app/src/components/JobCandidates.react.js
+++ react-app/src/components/JobCandidates.react.js
@@ -14,28 +14,6 @@ class JobCandidates extends React.Component {
 
     this.state = {
       num: 0,
-<<<<<<< HEAD
-      jobseekers: [
-  "userAbout": "",
-  "userCity": "",
-  "userEmail": "",
-  "userFirstName": " ",
-  "userLastName": "",
-  "userMiddleName": "",
-  "userPhoneNumber": "",
-  "userPostcode": "",
-  "userState": "",
-  "userType": ""
-],
-      skills: [
-  "userEmail": "",
-  "userSkills": [
-    "",
-    ""
-  ]
-],
-      filters: ["PHP"],
-=======
       currentCandidate: {
         userAbout: " ",
         userCity: " ",
@@ -48,72 +26,10 @@ class JobCandidates extends React.Component {
         userSkills: " ",
         userState: " ",
       },
->>>>>>> nick
     };
   }
 
   componentDidMount() {
-<<<<<<< HEAD
-    this.getJobseekers();
-    }
-
-    getJobseekers(){
-      fetch("https://8tyuiglaca.execute-api.ap-southeast-2.amazonaws.com/prod/")
-        .then(res => res.json())
-        .then((result) => {
-
-          let items = [];
-          for (var i = 0; i < result.length; i++){
-           if(result[i].userType === "jobseeker"){
-             items.push(result[i]);
-             }
-           }
-
-           this.setState(
-             {
-               jobseekers: items,
-             },
-             () => {
-               this.getBySkills();
-             }
-           );
-          },
-        )
-    }
-
-
-// code to pull users skills (based off user email) from the database
-
-getBySkills(){
-console.log(this.state.jobseekers);
-
-  fetch(`https://ezha2ns0bl.execute-api.ap-southeast-2.amazonaws.com/prod/userdata/skills?userEmail=${this.state.jobseekers[this.state.num].userEmail}`)
-    .then(res => res.json())
-    .then((result) => {
-      let items = [];
-    items.push(result.Item);
-
-    this.setState(
-      {
-        skills: items,
-      },
-      () => {
-        this.filterJobseekers();
-      }
-    );
-      }
-    )
-}
-
-
-filterJobseekers(){
-console.log(this.state.skills);
-//  if(this.state.filters.every(r => this.state.skills[0].userSkills.includes(r))){
-//
-// }
-
-}
-=======
     this.configureCandidates();
   }
 
@@ -182,25 +98,15 @@ console.log(this.state.skills);
       setTimeout(this.setCandidate, 250);
     }
   };
->>>>>>> nick
 
   filterJobseekers() {
     // WHEN FILTERING, LOOK THROUGH THE JOBSEEKER ARRAY FOR MATCHES. ANYTHING THAT DOESN'T MATCH, YOU CAN REMOVE FROM THE ARRAY USING 'jobseekers.splice(INDEX, 1)'
     // SPLICE SYNTAX IS: splice(position in array, amount of elements to remove)
 
-<<<<<<< HEAD
- acceptChanges = () => {
-   this.state.num++;
-   this.getBySkills();
-   console.log(this.state.num);
-   this.forceUpdate();
- };
-=======
     // if(this.state.filters.every(r => this.state.skills.Item.userSkills.includes(r))){
     //   items.push(result.Item);
     //
   }
->>>>>>> nick
 
   acceptChanges = () => {
     let newNum = this.state.num + 1;
@@ -220,73 +126,6 @@ console.log(this.state.skills);
     return (
       <Container>
         <Card>
-<<<<<<< HEAD
-         <Card.Header>
-           <Card.Title>{this.state.jobseekers[this.state.num].userFirstName} {this.state.jobseekers[this.state.num].userLastName}</Card.Title>
-         </Card.Header>
-         <Card.Body>
-          <Grid.Row>
-            <Grid.Col md={4}>
-              <Form.Group label="First Name">
-                <Form.Input name="firstname" readOnly value={this.state.jobseekers[this.state.num].userFirstName} />
-              </Form.Group>
-            </Grid.Col>
-            <Grid.Col md={4}>
-              <Form.Group label="Middle Name">
-                <Form.Input name="middlename" readOnly value={this.state.jobseekers[this.state.num].userMiddleName} />
-              </Form.Group>
-            </Grid.Col>
-            <Grid.Col md={4}>
-              <Form.Group label="Surname">
-                <Form.Input name="surname" readOnly value={this.state.jobseekers[this.state.num].userLastName}/>
-              </Form.Group>
-            </Grid.Col>
-            <Grid.Col sm={6} md={4}>
-              <Form.Group label="City">
-                <Form.Input name="City" readOnly value={this.state.jobseekers[this.state.num].userCity}/>
-              </Form.Group>
-            </Grid.Col>
-            <Grid.Col sm={6} md={3}>
-              <Form.Group label="Post Code">
-                <Form.Input name="Post Code" readOnly value={this.state.jobseekers[this.state.num].userPostcode}/>
-              </Form.Group>
-            </Grid.Col>
-            <Grid.Col md={5}>
-              <Form.Group label="Email" >
-                <Form.Input name="Email" readOnly value={this.state.jobseekers[this.state.num].userEmail}/>
-              </Form.Group>
-            </Grid.Col>
-            <Grid.Col md={12}>
-              <Form.Group className="mb=0" label="About Me">
-                <Form.Input name="About Me" readOnly value={this.state.jobseekers[this.state.num].userAbout}/>
-              </Form.Group>
-            </Grid.Col>
-            <Grid.Col md={12}>
-              <Form.Group className="mb=0" label="Skills">
-                <Form.Input name="Skills" readOnly value={this.state.skills[0].userSkills}/>
-              </Form.Group>
-            </Grid.Col>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Col md={12}>
-              <Button
-                floated="left"
-                negative
-                type="button"
-                color="red"
-                onClick={this.acceptChanges}
-              >
-                {" "}
-                Dislike{" "}
-              </Button>
-              <Button floated="right" positive type="submit" color="green" onClick={this.acceptChanges}>
-                {" "}
-                Like{" "}
-              </Button>
-            </Grid.Col>
-          </Grid.Row>
-        </Card.Body>
-=======
           <Card.Header>
             <Card.Title>
               {this.state.currentCandidate.userFirstName}
@@ -394,7 +233,6 @@ console.log(this.state.skills);
               </Grid.Col>
             </Grid.Row>
           </Card.Body>
->>>>>>> nick
         </Card>
       </Container>
     );
