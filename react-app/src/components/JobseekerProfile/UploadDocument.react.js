a4f9f022256652722fb694ad07378c105872c638
rmit-s3484836-william-spoljaric <30791550+rmit-s3484836-william-spoljaric@users.noreply.github.com>
2020-10-05 14:11:00 +1100

    work in progress document upload
    not yet working

--- react-app/src/components/JobseekerProfile/UploadDocument.react.js
+++ react-app/src/components/JobseekerProfile/UploadDocument.react.js
@@ -3,12 +3,28 @@ import { Card, Grid, Container } from "tabler-react";
 import { Button, Modal, Icon } from "semantic-ui-react";
 import Dropzone from "react-dropzone-uploader";
 import "react-dropzone-uploader/dist/styles.css";
+import S3FileUpload from 'react-s3';
+import reactS3, { uploadFile } from 'react-s3';
+
+const config = {
+  bucketName: 'skillshopdocs',
+  dirName: 'documents', /* optional */
+  region: 'ap-southeast-2',
+  accessKeyId: 'AKIAXUYWCMY6VO3N6LIK',
+  secretAccessKey: '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',
+}
 
 class UploadDocument extends React.Component {
   state = { files: [] };
-
-  getUploadParams = ({ meta }) => {
+   
+  upload(e, files, config){
+    
+  }
+  
+  getUploadParams = ({ meta}) => {
     // Need to do the upload functionality here
+    
+
     return { url: "https://httpbin.org/post" };
   };
 
@@ -19,7 +35,18 @@ class UploadDocument extends React.Component {
   handleSubmit = (files, allFiles) => {
     console.log(files.map((f) => f.meta));
     allFiles.forEach((f) => f.remove());
+       
   };
+
+  upload=(e, files)=>{
+    S3FileUpload.uploadFile(files[0], config)
+    .then((data)=> {
+        console.log(data.location)
+    })
+    .catch( (err)=>{
+        alert(err)
+    })
+  }
 
   render() {
     return (
@@ -30,7 +57,7 @@ class UploadDocument extends React.Component {
           <Dropzone
             getUploadParams={this.getUploadParams}
             onChangeStatus={this.handleChangeStatus}
-            onSubmit={this.handleSubmit}
+            onSubmit={this.upload}
             multiple={false}
             maxFiles={1}
             accept="*"
