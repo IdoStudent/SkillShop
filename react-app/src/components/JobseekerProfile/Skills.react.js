d8b672e027fa5fc0073c22a6d2f7c1911a70c05d
Gabe <s3601118@student.rmit.edu.au>
2020-10-18 14:52:45 +1100

    Fixed undifined skills error

--- react-app/src/components/JobseekerProfile/Skills.react.js
+++ react-app/src/components/JobseekerProfile/Skills.react.js
@@ -43,9 +43,11 @@ getFirstApi() {
     fetch(`https://ezha2ns0bl.execute-api.ap-southeast-2.amazonaws.com/prod/userdata/skills?userEmail=`+email)
       .then((res) => res.json())
       .then((result) => {
-
-        // We can just do a straight copy of the array we received into our var array
-        dbSkills = result.Item.userSkills;
+        console.log(result);
+        if (result.Item !== undefined) {
+          // We can just do a straight copy of the array we received into our var array
+          dbSkills = result.Item.userSkills;
+        }
 
         // After the skills are added into the array from the API, we set the state based on the given values
         this.initialiseState();
